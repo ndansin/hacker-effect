a34e85c0d4d273efa60205a24cbf08547972768e
Nina <nina.dansin@viragegroup.com>
2024-03-27 14:10:40 +0100

    Renault Senic

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.scss";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.querySelector<HTMLDivElement>("#app")!.onmouseover = (
+  event: MouseEvent
+) => {
+  let iteration = 0;
+  const letters = event?.target?.innerText.split("");
+  const fun = 3;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const interval = setInterval(() => {
+    if(fun + letters.length < iteration) {
+      clearInterval(interval);
+    }
+
+    event!.target!.innerText = letters
+    .map((letter, index) => {
+      if (" " === letter || fun + index < iteration) {
+        return letter;
+      }
+
+      return randomLetter();
+    })
+    .join("");
+    iteration++;
+  }, 35);
+
+};
+
+function randomLetter() {
+  return String.fromCharCode("A".charCodeAt() + Math.floor(Math.random() * 26));
+}
